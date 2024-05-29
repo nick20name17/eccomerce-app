45397e79d42ba8387b0819b6a070b38fcdd4a570
nazar <nick20name17@gmail.com>
2024-05-29 11:34:22 +0300

    add search

--- src/js/cards.js
+++ src/js/cards.js
@@ -3,15 +3,22 @@ import { deleteModalMarkup, editModal } from "./modals";
 import { trunc } from "./utils";
 
 export const renderCards = (products) => {
-  const cardList = document.querySelector("#card-list");
-  cardList.innerHTML = "";
+    const cardList = document.querySelector("#card-list");
+    cardList.innerHTML = "";
 
-  products.forEach((product) => {
-    const cardHTML = ` 
+    const isProductsEmpty = products.length === 0;
+
+    if (isProductsEmpty) {
+        cardList.innerHTML = `<h2>No products found</h2>`;
+        return;
+    }
+
+    products.forEach((product) => {
+        const cardHTML = ` 
             <div class="card" style="width: 18rem;">
                 <img src="${product.images[0]}" class="card-img-top" alt="${
-      product.title
-    }" />
+            product.title
+        }" />
                 <div class="card-body">
                     <h5 class="card-title">${product.title}</h5>
                     <p class="card-text">
@@ -26,20 +33,20 @@ export const renderCards = (products) => {
                 </div>
             </div>`;
 
-    cardList.insertAdjacentHTML("beforeend", cardHTML);
-  });
+        cardList.insertAdjacentHTML("beforeend", cardHTML);
+    });
 };
 
 export const handleProductActions = () => {
-  const deleteBtns = document.querySelectorAll(".delete-btn");
+    const deleteBtns = document.querySelectorAll(".delete-btn");
 
-  deleteBtns.forEach((btn) => {
-    btn.addEventListener("click", async () => {
-      const productId = btn.id;
-      await removeProduct(productId);
-      btn.closest(".card").remove();
-      document.querySelector(".modal-backdrop").remove();
-      document.body.style = "";
+    deleteBtns.forEach((btn) => {
+        btn.addEventListener("click", async () => {
+            const productId = btn.id;
+            await removeProduct(productId);
+            btn.closest(".card").remove();
+            document.querySelector(".modal-backdrop").remove();
+            document.body.style = "";
+        });
     });
-  });
 };
--- src/js/form-handlers.js
+++ src/js/form-handlers.js
@@ -3,61 +3,67 @@ import { renderCards } from "./cards";
 import { debounce } from "./utils";
 
 export const addProductFormHandler = () => {
-  const addProductForm = document.querySelector("#add-product-form");
+    const addProductForm = document.querySelector("#add-product-form");
 
-  addProductForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const title = document.querySelector("#product-title").value;
-    const description = document.querySelector("#product-descr").value;
-    const price = +document.querySelector("#product-price").value;
-    const categoryId = +document.querySelector("#product-category").value;
+    addProductForm.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const title = document.querySelector("#product-title").value;
+        const description = document.querySelector("#product-descr").value;
+        const price = +document.querySelector("#product-price").value;
+        const categoryId = +document.querySelector("#product-category").value;
 
-    const productToAdd = {
-      title,
-      description,
-      price,
-      categoryId,
-      images: ["https://placeimg.com/640/480/any"],
-    };
+        const productToAdd = {
+            title,
+            description,
+            price,
+            categoryId,
+            images: ["https://placeimg.com/640/480/any"],
+        };
 
-    await addProduct(productToAdd);
-  });
+        await addProduct(productToAdd).then((data) => renderCards(data));
+    });
 };
 
 export const editProductFormHandler = () => {
-  const editProductForms = document.querySelectorAll("#edit-product-form");
-  editProductForms.forEach((editProductForm) => {
-    editProductForm.addEventListener("submit", async (e) => {
-      e.preventDefault();
+    const editProductForms = document.querySelectorAll("#edit-product-form");
+    editProductForms.forEach((editProductForm) => {
+        editProductForm.addEventListener("submit", async (e) => {
+            e.preventDefault();
 
-      const title = document.querySelector("#edit-product-title").value;
-      const description = document.querySelector("#edit-product-descr").value;
-      const price = +document.querySelector("#edit-product-price").value;
-      const categoryId = +document.querySelector("#edit-product-category")
-        .value;
+            const title = document.querySelector("#edit-product-title").value;
+            const description = document.querySelector(
+                "#edit-product-descr"
+            ).value;
+            const price = +document.querySelector("#edit-product-price").value;
+            const categoryId = +document.querySelector("#edit-product-category")
+                .value;
 
-      const productToEdit = {
-        title,
-        description,
-        price,
-        categoryId,
-        images: ["https://placeimg.com/640/480/any"],
-      };
+            const productToEdit = {
+                title,
+                description,
+                price,
+                categoryId,
+                images: ["https://placeimg.com/640/480/any"],
+            };
 
-      await editProduct(editProductForm.dataset.product, productToEdit);
+            await editProduct(
+                editProductForm.dataset.product,
+                productToEdit
+            ).then((data) => renderCards(data));
+        });
     });
-  });
 };
 
 export const searchProductFormHandler = async () => {
-  const searchInput = document.querySelector("#search");
+    const searchInput = document.querySelector("#search");
 
-  const debouncedGetProducts = debounce(
-    async (searchTerm) => await getProducts(searchTerm)
-  );
+    const debouncedGetProducts = debounce(
+        async (searchTerm) =>
+            await getProducts(searchTerm).then((data) => renderCards(data))
+    );
 
-  searchInput.addEventListener("input", async (e) => {
-    const searchTerm = e.target.value;
-    debouncedGetProducts(searchTerm);
-  });
+    searchInput.addEventListener("input", async (e) => {
+        const searchTerm = e.target.value;
+        debouncedGetProducts(searchTerm);
+    });
 };
